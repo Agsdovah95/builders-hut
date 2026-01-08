610498a7ceb9258b7b20329d6fcf87bfacdf8768
r3Vibe <arnabgupta84@gmail.com>
2026-01-08 14:26:43 +0530

    initial code

---
+++ builders_hut/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to Builders Hut!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from builders-hut!")
+
+
+if __name__ == "__main__":
+    main()
